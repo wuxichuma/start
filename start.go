d33637273760a08c75ef588cb21950512d6839c6
wuxichuma <5797564@qq.com>
2019-11-08 11:45:00 +0800

    new

--- start.go
+++ start.go
@@ -5,4 +5,10 @@ import "fmt"
 func main() {
 	fmt.Print("wuxi\n")
 	fmt.Print("wuxi2\n")
+	for index := 0; index < 10; index++ {
+		fmt.Print(index)
+	}
+	ch := make(chan int, 2)
+	ch <- 2
+	fmt.Println(<-ch)
 }
